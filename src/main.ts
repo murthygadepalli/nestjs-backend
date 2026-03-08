261edc909e4e5257edeb48e410ee41808f5bfdb5
bhavani0747 <durgabhavani0747@gmail.com>
2026-03-08 17:02:14 +0530

    hello

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
 
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Server running on port ${port}`);
+
 
 }
+
+
 bootstrap();
