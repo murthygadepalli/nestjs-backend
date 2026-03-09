fa99987ddd7198d6a124fca448f50732f71e42eb
bhavani0747 <durgabhavani0747@gmail.com>
2026-03-09 22:12:12 +0530

    d

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
 
 async function bootstrap() {
 
@@ -8,7 +7,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
 
   app.enableCors();
-app.use('/uploads', express.static('uploads'));
+
   await app.listen(port);
   console.log(`Server running on port ${port}`);
 
